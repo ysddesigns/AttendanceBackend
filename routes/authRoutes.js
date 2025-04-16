7255567b266c23b2c12306a40fb2d8b60ea90d99
ysd designs <yusufbyusufgwarmai.com>
2025-04-16 13:47:18 +0100

    update routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
 
-router.post("/register", register);
+router.post("/signup", register);
 router.post("/login", login);
 
 module.exports = router;
